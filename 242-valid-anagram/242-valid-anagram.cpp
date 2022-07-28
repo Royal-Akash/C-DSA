6bef4e9717eec624cc9c7aa574c2ca1e733d7942
Akash Jha <akashjha412@gmail.com>
2022-07-28 17:43:07 +0530

    Time: 34 ms (5.61%), Space: 7.3 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,21 +2,42 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        sort(s.begin() , s.end());
-        sort(t.begin() , t.end());
+//         sort(s.begin() , s.end());
+//         sort(t.begin() , t.end());
+        
+//         if(s.size()!=t.size()){
+//             return false;
+//         }
+        
+//         for(int i=0;i<s.size();i++){
+            
+//             if(s[i]!=t[i]){
+//                 return false;
+//             }
+            
+//         }
+        
+//         return true;
         
         if(s.size()!=t.size()){
             return false;
         }
         
-        for(int i=0;i<s.size();i++){
-            
-            if(s[i]!=t[i]){
-                return false;
-            }
-            
-        }
+        map<char , int> mp1 , mp2;
         
+        for(auto c : s){
+            mp1[c]++;
+        }
+        for(auto y : t){
+            mp2[y]++;
+        }
+            
+            for(int i=0;i<s.size();i++){
+                
+                if(mp1[s[i]]!=mp2[s[i]]){
+                    return false;
+                }
+            }
         return true;
     }
 };
