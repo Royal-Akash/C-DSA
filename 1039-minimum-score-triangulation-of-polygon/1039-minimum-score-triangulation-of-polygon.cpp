eedb147ba835bd03d5eb2c88da9a10873db148a4
Akash Jha <akashjha412@gmail.com>
2022-08-17 12:26:25 +0530

    Time: 49 ms (5.03%), Space: 12.5 MB (5.03%) - LeetHub

---
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int findsoln(vector<int>& values , int i , int j){
+        
+        if(i>=j){return 0;}
+        if(dp[i][j]!=-1) {return dp[i][j];}
+        
+        dp[i][j]=INT_MAX;
+        // int count = INT_MAX;
+        for(int k=i;k<=j-1;k++){
+            
+            dp[i][j] = min(dp[i][j] , findsoln(values , i , k)+findsoln(values , k+1 , j)+
+                values[i-1]*values[k]*values[j]);
+            
+            // count=min(count , temp);
+            
+            // int temp = findsoln(values , i , k)+findsoln(values , k+1 , j)+
+            //     values[i-1]*values[k]*values[j];
+            // count=min(count , temp);
+
+        }
+        // dp[i][j]=count;
+        // return dp[i][j]=count;
+         return dp[i][j];
+    }
+    
+    int minScoreTriangulation(vector<int>& values) {
+        
+        int n = values.size();
+        int i=0 , j=n-1;
+        memset(dp , -1 , sizeof(dp));
+        return findsoln(values , 1 , n-1);
+        
+    }
+};
