1f2a192d598e3cfe2efccd12f2289a143b298160
Akash Jha <akashjha412@gmail.com>
2022-08-23 13:54:40 +0530

    Time: 272 ms (84.14%), Space: 118 MB (59.47%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -8,48 +8,78 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+// class Solution {
+// public:
+    
+//     ListNode * reverse(ListNode* head){
+//         ListNode * curr=head;
+//         ListNode * prev = NULL;
+//         ListNode * forw = NULL;
+//         while(curr!=NULL){
+//             forw = curr->next;
+//             curr->next = prev;
+//             prev=curr;
+//             curr=forw;
+//          }
+//             return prev;
+//         }
+//     bool isPalindrome(ListNode* head) {
+        
+//         if(head==NULL || head->next==NULL) return true;
+//         ListNode*slow = head;
+//         ListNode * fast = head;
+        
+//         while(fast->next!=NULL && fast->next->next!=NULL){
+//             slow=slow->next;
+//             fast=fast->next->next;
+//         }
+//         slow->next=reverse(slow->next);
+//         slow=slow->next;
+        
+//         while(slow!=NULL){
+//             if(head->val!=slow->val) return false;
+//             else{
+//                 head=head->next;
+//                 slow=slow->next;
+//             }
+//         }
+//         return true;
+//       }
+// };      
+        
+        class Solution {
 public:
-    
-    ListNode * reverse(ListNode* head){
-        ListNode * curr=head;
-        ListNode * prev = NULL;
-        ListNode * forw = NULL;
-        while(curr!=NULL){
-            forw = curr->next;
-            curr->next = prev;
-            prev=curr;
-            curr=forw;
-         }
-            return prev;
-        }
-    
-    
     bool isPalindrome(ListNode* head) {
-        
-        if(head==NULL || head->next==NULL) return true;
-        ListNode*slow = head;
-        ListNode * fast = head;
-        
-        while(fast->next!=NULL && fast->next->next!=NULL){
+        if(head==NULL||head->next==NULL)
+            return true;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL&&fast->next->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
         }
-        slow->next=reverse(slow->next);
+        slow->next=reverseList(slow->next);
         slow=slow->next;
-        
         while(slow!=NULL){
-            if(head->val!=slow->val) return false;
-            else{
-                head=head->next;
-                slow=slow->next;
-            }
+            if(head->val!=slow->val)
+                return false;
+            head=head->next;
+            slow=slow->next;
         }
         return true;
-      }
-};      
-        
-        
+    }
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre=NULL;
+        ListNode* next=NULL;
+        while(head!=NULL){
+            next=head->next;
+            head->next=pre;
+            pre=head;
+            head=next;
+        }
+        return pre;
+    }
+};
         
         
         
