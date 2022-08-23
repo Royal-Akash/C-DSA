f41e714b67f0e688466167932d5898d364431c99
Akash Jha <akashjha412@gmail.com>
2022-08-23 13:42:49 +0530

    Time: 424 ms (34.25%), Space: 117.9 MB (59.47%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -0,0 +1,87 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode * reverse(ListNode* head){
+        ListNode * curr=head;
+        ListNode * prev = NULL;
+        ListNode * forw = NULL;
+        while(curr!=NULL){
+            forw = curr->next;
+            curr->next = prev;
+            prev=curr;
+            curr=forw;
+         }
+            return prev;
+        }
+    
+    
+    bool isPalindrome(ListNode* head) {
+        
+        if(head==NULL || head->next==NULL) return true;
+        ListNode*slow = head;
+        ListNode * fast = head;
+        
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        slow->next=reverse(slow->next);
+        slow=slow->next;
+        
+        while(slow!=NULL){
+            if(head->val!=slow->val) return false;
+            else{
+                head=head->next;
+                slow=slow->next;
+            }
+        }
+        return true;
+      }
+};      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         // curr=prev;
+//         // while(prev!=NULL){
+//         //     cout<<prev->val;
+//         //     prev=prev->next;
+//         // }cout<<endl;
+//         // prev=curr;
+//         // curr=head;
+        
+//         while(full!=NULL){
+            
+//             if(full->val!=prev->val){
+//                 return false;
+//                 break;
+//             }
+//             else{
+//                 full=full->next;
+//                 prev=prev->next;
+//             }
+//         }
+//         return true;
