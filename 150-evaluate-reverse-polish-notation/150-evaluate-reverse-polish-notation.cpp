2af8fba7a74568d4e7baa0587871f2d2c808af4f
Akash Jha <akashjha412@gmail.com>
2022-08-25 10:21:24 +0530

    Time: 20 ms (38.51%), Space: 12 MB (19.62%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -2,51 +2,76 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         
-        stack<int> s;
-        int n = tokens.size();
-        int sum=0;
-        for(int i=0;i<n;i++){
+//         stack<int> s;
+//         int n = tokens.size();
+//         int sum=0;
+//         for(int i=0;i<n;i++){
             
-            if(tokens[i]=="+"){
-                int y = (s.top());
-                s.pop();
-                int z = (s.top());
-                s.pop();
-                int sum=z+y;
-                s.push(sum);
-            }
-            else if(tokens[i]=="-"){
-                int y = (s.top());
-                s.pop();
-                int z = (s.top());
-                s.pop();
-                int sum=z-y;
-                s.push(sum);
-            }
-            else if(tokens[i]=="*"){
-                int y = (s.top());
-                s.pop();
-                int z = (s.top());
-                s.pop();
-                int sum=z*y;
-                s.push(sum);
-            }
-            else if(tokens[i]=="/"){
-                int y = (s.top());
-                s.pop();
-                int z = (s.top());
-                s.pop();
-                int sum=z/y;
-                s.push(sum);
+//             if(tokens[i]=="+"){
+//                 int y = (s.top());
+//                 s.pop();
+//                 int z = (s.top());
+//                 s.pop();
+//                 int sum=z+y;
+//                 s.push(sum);
+//             }
+//             else if(tokens[i]=="-"){
+//                 int y = (s.top());
+//                 s.pop();
+//                 int z = (s.top());
+//                 s.pop();
+//                 int sum=z-y;
+//                 s.push(sum);
+//             }
+//             else if(tokens[i]=="*"){
+//                 int y = (s.top());
+//                 s.pop();
+//                 int z = (s.top());
+//                 s.pop();
+//                 int sum=z*y;
+//                 s.push(sum);
+//             }
+//             else if(tokens[i]=="/"){
+//                 int y = (s.top());
+//                 s.pop();
+//                 int z = (s.top());
+//                 s.pop();
+//                 int sum=z/y;
+//                 s.push(sum);
+//             }
+//             else{
+//                 int num=stoi(tokens[i]);
+//                 s.push(num);
+//             }
+            
+//         }
+//         int k = s.top();
+//         return k;
+        
+        
+        unordered_map<string , function<int (int , int)>> map = {
+            
+            {"+" , [] (int a , int b) {return a+b;}},
+            {"-" , [] (int a , int b) {return a-b;}},
+            {"*" , [] (int a , int b) {return a*b;}},
+            {"/" , [] (int a , int b) {return a/b;}}
+            
+        };
+        stack<int> st;
+        for(auto &it : tokens){
+            if(!map.count(it)){
+                st.push(stoi(it));
             }
             else{
-                int num=stoi(tokens[i]);
-                s.push(num);
+                int x1 = st.top();
+                st.pop();
+                int x2 = st.top();
+                st.pop();
+                
+                st.push(map[it](x2 , x1));
+                
             }
-            
         }
-        int k = s.top();
-        return k;
-        
+        return st.top();
     }
 };
