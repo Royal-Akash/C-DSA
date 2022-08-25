76d158b44672508799658b81c44c3bb8719b856e
Akash Jha <akashjha412@gmail.com>
2022-08-25 12:09:32 +0530

    Time: 32 ms (39.48%), Space: 9 MB (15.41%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,21 +2,29 @@ class Solution {
 public:
     bool canConstruct(string r, string m){
         
-        // unordered_map<char , int> mp;
-        // for(int i=0;i<m.size();i++){
-        //     mp[m[i]]++;
-        // }
-        vector<int> mp(26 , 0);
+        unordered_map<char , int> mp(26);
         for(int i=0;i<m.size();i++){
-            mp[m[i]-'a']++;
+            mp[m[i]]++;
         }
-        
         for(int i=0;i<r.size();i++){
-            if(mp[r[i]-'a']==0) return false;
+            if(mp[r[i]]==0) return false;
             else{
-                mp[r[i]-'a']--;
+                mp[r[i]]--;
             }
         }
         return true;
+        
+//         vector<int> mp(26 , 0);
+//         for(int i=0;i<m.size();i++){
+//             mp[m[i]-'a']++;
+//         }
+        
+//         for(int i=0;i<r.size();i++){
+//             if(mp[r[i]-'a']==0) return false;
+//             else{
+//                 mp[r[i]-'a']--;
+//             }
+//         }
+//         return true;
     }
 };
