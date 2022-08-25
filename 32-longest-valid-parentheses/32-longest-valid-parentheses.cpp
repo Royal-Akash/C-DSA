c4a0f9662c710325e24d66cdcd11666457b2b6c9
Akash Jha <akashjha412@gmail.com>
2022-08-25 19:01:01 +0530

    Time: 3 ms (91.69%), Space: 7.3 MB (41.97%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,23 +1,55 @@
+// class Solution {
+// public:
+//     int longestValidParentheses(string s) {
+        
+//         int n = s.size();
+//         int count=0;
+//         stack<char>st;
+//         stack<int> ind;
+//         ind.push(-1);
+        
+//         for(int i=0;i<n;i++){
+            
+//             if(s[i]=='('){
+//                 st.push(s[i]);
+//                 ind.push(i);
+//             }
+//             else{
+//                 if(!st.empty() && st.top()=='('){
+//                     st.pop();
+//                     ind.pop();
+//                     count=max(count , i-ind.top());
+//                 }
+//                 else{
+//                     ind.push(i);
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int longestValidParentheses(string s) {
         
         int n = s.size();
         int count=0;
-        stack<char>st;
+        // stack<char>st;
         stack<int> ind;
         ind.push(-1);
         
         for(int i=0;i<n;i++){
             
             if(s[i]=='('){
-                st.push(s[i]);
+                // st.push(s[i]);
                 ind.push(i);
             }
             else{
-                if(!st.empty() && st.top()=='('){
-                    st.pop();
-                    ind.pop();
+                ind.pop();
+                if(!ind.empty()){
+                    // st.pop();
+                    
                     count=max(count , i-ind.top());
                 }
                 else{
