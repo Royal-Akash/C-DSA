3194ffc7476b1e133a94e4d18f00b3b34e1f35fe
Akash Jha <akashjha412@gmail.com>
2022-08-25 18:51:50 +0530

    Time: 8 ms (44.82%), Space: 7.6 MB (16.13%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        
+        int n = s.size();
+        int count=0;
+        stack<char>st;
+        stack<int> ind;
+        ind.push(-1);
+        
+        for(int i=0;i<n;i++){
+            
+            if(s[i]=='('){
+                st.push(s[i]);
+                ind.push(i);
+            }
+            else{
+                if(!st.empty() && st.top()=='('){
+                    st.pop();
+                    ind.pop();
+                    count=max(count , i-ind.top());
+                }
+                else{
+                    ind.push(i);
+                }
+            }
+        }
+        return count;
+    }
+};
