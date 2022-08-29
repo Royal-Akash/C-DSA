4c44377f59c77611060c8ee63e14d7e7ad9e5a5d
Akash Jha <akashjha412@gmail.com>
2022-08-29 12:14:26 +0530

    Time: 3 ms (58.41%), Space: 7.9 MB (63.83%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,58 +1,59 @@
 class Solution {
-//     int find(vector<int>& nums , int k , int j){
+    int find(vector<int>& nums , int k , int j){
         
-//         int dp[j];
-//         dp[0]=nums[k];
-//         if((j-k)==1) return nums[k];
-//         for(int i=k+1;i<j;i++){
+        if((j-k)==1) return nums[k];
+        int prev = nums[k];
+        int prev2=0;
+        for(int i=k+1;i<j;i++){
             
-//             int pick = nums[i];
-//             if(i>k+1){
-//                 pick+=dp[i-2];
-//             }
-            
-//             int nopick = dp[i-1];
-//             dp[i]=max(pick , nopick);
-//         }
-//         return dp[j-1];
-//     }
+            int pick = nums[i];
+            if(i>k+1){
+                pick+=prev2;
+            }
+            int nopick= prev;
+            int curr = max(pick , nopick);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
     
 public:
-//     int rob(vector<int>& nums){
-//         int n = nums.size();
-//         if(n==1) return nums[0];
-//         int num1 = find(nums , 0 , n-1);
-//         int num2 = find(nums, 1 , n);
-//         cout<<num1<<" "<<num2<<endl;
-//         return max(num1 , num2);
+    int rob(vector<int>& nums){
+        int n = nums.size();
+        if(n==1) return nums[0];
+        int num1 = find(nums , 0 , n-1);
+        int num2 = find(nums, 1 , n);
+        cout<<num1<<" "<<num2<<endl;
+        return max(num1 , num2);
         
+        
+    }
+};
+
+// int robhelp(int start,int end,vector<int> &nums) {
+//         int prev1=nums[start];
+//         int prev2=0,curr;
+//         int fs,ss;
+//         if(end-start==1)
+//             return nums[start];
+//         for(int i=start+1;i<end;i++)
+//         {
+//            fs=nums[i];
+//            if(i>start+1)
+//              fs+=prev2;
+//            ss=prev1;
+//            curr=max(fs,ss);
+//             prev2=prev1;
+//             prev1=curr;
+//         }
+//         return curr;
+//     }
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==1)
+//             return nums[0];
+//        return max(robhelp(0,n-1,nums),robhelp(1,n,nums)) ;
         
 //     }
-// };
-
-int robhelp(int start,int end,vector<int> &nums) {
-        int prev1=nums[start];
-        int prev2=0,curr;
-        int fs,ss;
-        if(end-start==1)
-            return nums[start];
-        for(int i=start+1;i<end;i++)
-        {
-           fs=nums[i];
-           if(i>start+1)
-             fs+=prev2;
-           ss=prev1;
-           curr=max(fs,ss);
-            prev2=prev1;
-            prev1=curr;
-        }
-        return curr;
-    }
-    int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)
-            return nums[0];
-       return max(robhelp(0,n-1,nums),robhelp(1,n,nums)) ;
-        
-    }
-    };
+//     };
