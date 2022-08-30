8fd732e778e772381e54298b0c0550603f6f90ec
Akash Jha <akashjha412@gmail.com>
2022-08-30 12:41:41 +0530

    Time: 0 ms (100.00%), Space: 7 MB (73.79%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         
-        vector<int> ans;
-        int row=matrix.size();
-        int col=matrix[0].size();
+//         vector<int> ans;
+//         int row=matrix.size();
+//         int col=matrix[0].size();
         
-        int startingRow=0;
-        int startingCol=0;
-        int endingRow=row-1;
-        int endingCol=col-1;
-        int count=0;
+//         int startingRow=0;
+//         int startingCol=0;
+//         int endingRow=row-1;
+//         int endingCol=col-1;
+//         int count=0;
         
-        int total=row*col;
+//         int total=row*col;
         
 //         for(int j=startingRow; j<=endingRow;j++){
             
@@ -21,14 +21,16 @@ public:
 //                 swap(matrix[i][j], matrix[j][i]);
                 
 //             }
-            // endingCol--;
+//             endingCol--;
             
-            // for(int i=startingRow; i<=endingRow; i++){
-            //     ans.push_back(matrix[startingRow][i]);
-            //     count++;
-            // }
-            // startingRow++;
-        // }
+//             for(int i=startingRow; i<=endingRow; i++){
+//                 ans.push_back(matrix[startingRow][i]);
+//                 count++;
+//             }
+//             startingRow++;
+//         }
+        
+        
         for(int i=0;i<matrix.size();i++){
             for(int j=i;j<matrix.size();j++){
                 int temp = matrix[i][j];
