993cb991fe257b65a9419efe5f0f3089f6912968
Akash Jha <akashjha412@gmail.com>
2022-09-05 13:21:21 +0530

    Time: 25 ms (44.66%), Space: 9 MB (40.43%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int find(int n , int m , string & word1 , string & word2 , vector<vector<int>> &dp){
         
-        if(n<0) return m+1;
-        if(m<0) return n+1;
+        if(n==0) return m;
+        if(m==0) return n;
         
         if(dp[n][m]!=-1) return dp[n][m];
         
@@ -16,36 +16,44 @@ public:
         
     }
     
-//             int editDistanceUtil(string& S1, string& S2, int i, int j, vector<vector<int>>& dp){
-
-//             if(i<0)
-//                 return j+1;
-//             if(j<0)
-//                 return i+1;
-
-//             if(dp[i][j]!=-1) return dp[i][j];
-
-//             if(S1[i]==S2[j])
-//                 return dp[i][j] =  0+editDistanceUtil(S1,S2,i-1,j-1,dp);
-
-//             // Minimum of three choices
-//             else return dp[i][j] = 1+min(editDistanceUtil(S1,S2,i-1,j-1,dp),
-//             min(editDistanceUtil(S1,S2,i-1,j,dp),editDistanceUtil(S1,S2,i,j-1,dp)));
-
-//         }
-    
     
     int minDistance(string word1, string word2) {
         
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>>dp(n , vector<int>(m , -1));
-        return find(n-1 , m-1 , word1, word2 , dp);
+        vector<vector<int>>dp(n+1 , vector<int>(m+1 , 0));
+        
+//         if(n==0) return m;
+//         if(m==0) return n;
+        
+//         if(dp[n][m]!=-1) return dp[n][m];
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i]=i;
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    
+                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i][j-1] ,dp[i-1][j]));
+                    
+                }
+            }
+            
+        }
+        
+        
+        
+        return dp[n][m];
           
-//             int n = S1.size();
-//             int m = S2.size();
 
-//             vector<vector<int>> dp(n,vector<int>(m,-1));
-//             return editDistanceUtil(S1,S2,n-1,m-1,dp);
     }
 };
