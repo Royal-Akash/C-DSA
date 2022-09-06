9df8462bcb625cd3e49347a37c8862ddff4af5cd
Akash Jha <akashjha412@gmail.com>
2022-09-06 12:48:49 +0530

    Time: 160 ms (21.76%), Space: 11.2 MB (70.60%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool findstar(int i , string& p){
+//     bool findstar(int i , string& p){
         
-        for(int j=1;j<=i;j++){
-            if(p[j-1]!='*'){
-                return false;
-            }
-        }
-        return true;
-    }
+//         for(int j=1;j<=i;j++){
+//             if(p[j-1]!='*'){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
     
     bool isMatch(string s, string p) {
         
@@ -18,23 +18,23 @@ public:
         vector<vector<bool>> dp(n+1  , vector<bool>(m+1 , false));
         
         dp[0][0]=true;
-        for(int j=1;j<=m;j++){
-            dp[0][j]=findstar(j , p);
-        }
-        for(int j=1;j<=n;j++){
-            dp[j][0]=false;
-        }
+        // for(int j=1;j<=m;j++){
+        //     dp[0][j]=findstar(j , p);
+        // }
+        // for(int j=1;j<=n;j++){
+        //     dp[j][0]=false;
+        // }
         
-//         for(int j=1;j<=m;j++){
-//             bool flag = true;
-//             for(int k=1;k<=j;k++){
-//                 if(p[k-1] != '*'){
-//                     flag = false;
-//                     break;
-//                 }
-//             }
-//             dp[0][j] = flag;
-//         }
+        for(int j=1;j<=m;j++){
+            bool flag = true;
+            for(int k=1;k<=j;k++){
+                if(p[k-1] != '*'){
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][j] = flag;
+        }
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
