5d966b67d8073e86baec8cfefb56d88ebe767771
Akash Jha <akashjha412@gmail.com>
2022-09-07 18:21:32 +0530

    Time: 452 ms (48.92%), Space: 135.2 MB (44.07%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,22 +3,39 @@ public:
     int maxProfit(vector<int>& prices){
        
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1 , vector<vector<int>>(2 , vector<int>(3, 0)));
+//         vector<vector<vector<int>>> dp(n+1 , vector<vector<int>>(2 , vector<int>(3, 0)));
         
+//         for(int i=n-1;i>=0;i--){
+//             for(int j=0;j<2;j++){
+//                 for(int k=1;k<=2;k++){
+                    
+//                     if(j==0){
+//                         dp[i][j][k]=max(-prices[i]+dp[i+1][1][k] , dp[i+1][0][k]);
+//                     }
+//                     else{
+//                         dp[i][j][k]=max(prices[i]+dp[i+1][0][k-1] , dp[i+1][1][k]);
+//                     }
+                    
+//                 }
+//             }
+//         }
+//        return dp[0][0][2];
+        
+        vector<vector<int>>dp(n+1 , vector<int>(5 , 0));
+        int profit=0;
         for(int i=n-1;i>=0;i--){
-            for(int j=0;j<2;j++){
-                for(int k=1;k<=2;k++){
-                    
-                    if(j==0){
-                        dp[i][j][k]=max(-prices[i]+dp[i+1][1][k] , dp[i+1][0][k]);
-                    }
-                    else{
-                        dp[i][j][k]=max(prices[i]+dp[i+1][0][k-1] , dp[i+1][1][k]);
-                    }
-                    
+            for(int j=3;j>=0;j--){
+                
+                if(j%2==0){
+                    profit=max(-prices[i]+dp[i+1][j+1] , dp[i+1][j]);
                 }
+                else{
+                    profit=max(prices[i]+dp[i+1][j+1] , dp[i+1][j]);
+                }
+                dp[i][j]=profit;
             }
         }
-       return dp[0][0][2];
+        return dp[0][0];
+        
     }
 };
