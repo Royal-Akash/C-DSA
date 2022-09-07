5abdee8dc913adf9fdc93047ac3a75d281153583
Akash Jha <akashjha412@gmail.com>
2022-09-07 18:52:16 +0530

    Time: 20 ms (50.58%), Space: 13 MB (37.98%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& prices) {
+    int maxProfit(int a, vector<int>& prices) {
         
         int n = prices.size();
-        int z = 2*k+1;
-        vector<vector<int>>dp(n+1 , vector<int>(z , 0));
-        int profit=0;
-        for(int i=n-1;i>=0;i--){
-            for(int j=z-2;j>=0;j--){
+        // int z = 2*k+1;
+//         vector<vector<int>>dp(n+1 , vector<int>(z , 0));
+//         int profit=0;
+//         for(int i=n-1;i>=0;i--){
+//             for(int j=z-2;j>=0;j--){
                 
-                if(j%2==0){
-                    profit=max(-prices[i]+dp[i+1][j+1] , dp[i+1][j]);
+//                 if(j%2==0){
+//                     profit=max(-prices[i]+dp[i+1][j+1] , dp[i+1][j]);
+//                 }
+//                 else{
+//                     profit=max(prices[i]+dp[i+1][j+1] , dp[i+1][j]);
+//                 }
+//                 dp[i][j]=profit;
+//             }
+//         }
+//         return dp[0][0];
+        
+        vector<vector<vector<int>>> dp(n+1 , vector<vector<int>>(2 , vector<int>(a+1, 0)));
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                for(int k=1;k<=a;k++){
+                    
+                    if(j==0){
+                        dp[i][j][k]=max(-prices[i]+dp[i+1][1][k] , dp[i+1][0][k]);
+                    }
+                    else{
+                        dp[i][j][k]=max(prices[i]+dp[i+1][0][k-1] , dp[i+1][1][k]);
+                    }
+                    
                 }
-                else{
-                    profit=max(prices[i]+dp[i+1][j+1] , dp[i+1][j]);
-                }
-                dp[i][j]=profit;
             }
         }
-        return dp[0][0];
+       return dp[0][0][a];
         
     }
 };
