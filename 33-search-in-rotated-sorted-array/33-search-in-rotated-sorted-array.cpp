5800a2e1c9eb6d6e9e805e57f8828677de1b62a0
Akash Jha <akashjha412@gmail.com>
2022-09-09 01:39:14 +0530

    Time: 7 ms (54.67%), Space: 11.1 MB (74.96%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -2,13 +2,42 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         
-        auto it = find(nums.begin() , nums.end() , target);
-            if(it!=nums.end()){
-                return it-nums.begin();
-            }
-        else{
-            return -1;
-        }
+        // auto it = find(nums.begin() , nums.end() , target);
+        //     if(it!=nums.end()){
+        //         return it-nums.begin();
+        //     }
+        // else{
+        //     return -1;
+        // }
         
+        int n = nums.size();
+        int left=0;
+        int right=n-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[left]<=nums[mid]){
+                
+                if(nums[left]<=target && target<=nums[mid]){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+                
+            }
+            else{
+                
+                if(nums[mid]<=target && target<=nums[right]){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
+                
+            }
+            
+        }
+        return -1;
     }
 };
