cc4d4c41d984ed2f949c210f05557bdc7536760c
Akash Jha <akashjha412@gmail.com>
2022-09-12 12:46:56 +0530

    Time: 24 ms (9.93%), Space: 9.5 MB (87.72%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+     ////////////////////////Using Set Bits ///////////////////////////   
+        // unsigned int shift=1 , ans=0;
+        // for(int i=0;i<32;i++){
+        //     int count =0;
+        //     for(auto it:nums){
+        //         if(it&shift){
+        //             count++;
+        //         }
+        //     }
+        //     if(count%3!=0){
+        //         ans+=shift;
+        //     }
+        //     shift<<=1;
+        // }
+        // return ans;
         
-        unsigned int shift=1 , ans=0;
-        for(int i=0;i<32;i++){
-            int count =0;
-            for(auto it:nums){
-                if(it&shift){
-                    count++;
-                }
-            }
-            if(count%3!=0){
-                ans+=shift;
-            }
-            shift<<=1;
-        }
-        return ans;
-//         unsigned int ans=0,shift=1;
-//         for(int i=0;i<32;++i)
-//         {
-//             int count=0;
-//             for(auto ele: nums)
-//             {
-//                 if(ele & shift)
-//                     count+=1;
-//             }
-//             if(count%3 != 0)
-//                 ans += shift;
-//             shift*=2;
+        ///// TC = O(n*32);
+        
+        ///////////////////////// Sorting Technique ///////////////////////
+        int n = nums.size();
+        sort(nums.begin() , nums.end());
+        int ans=0;
+        if(n==1) return nums[n-1];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        for(int i=1;i<nums.size();i++){
             
-//         }
-//         return ans;
+            if(nums[i]==nums[i-1]){
+                i+=2;
+            }
+            else{
+                ans= nums[i-1];
+                break;
+            }
+        }
+            return ans;
     }
 };
