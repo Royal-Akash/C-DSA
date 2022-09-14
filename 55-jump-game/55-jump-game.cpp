ed19392242a3caa8cd99198a6007ed8e96517ba9
Akash Jha <akashjha412@gmail.com>
2022-09-14 14:32:23 +0530

    Time: 174 ms (26.56%), Space: 48.3 MB (77.79%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -3,11 +3,19 @@ public:
     bool canJump(vector<int>& nums) {
         int n = nums.size()-1;
         
-        for(int i=n;i>=0;i--){
-            if(i+nums[i]>=n){
-                n=i;
+        // for(int i=n;i>=0;i--){
+        //     if(i+nums[i]>=n){
+        //         n=i;
+        //     }
+        // }
+        // return n==0;
+        int reach=0;
+        for(int i=0;i<=n;i++){
+            if(reach<i){
+                return false;
             }
+            reach=max(reach , i+nums[i]);
         }
-        return n==0;
+        return true;
     }
 };
