0e6dea7c0ea049a9d335ce5f1536a15d34509707
Akash Jha <akashjha412@gmail.com>
2022-09-21 11:24:46 +0530

    Time: 6 ms (84.82%), Space: 8.9 MB (32.24%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -4,25 +4,25 @@ public:
         
         int n = nums2.size();
         int m = nums1.size();
-        vector<int>ans;
+        // vector<int>ans;
         unordered_map<int , int>nxt;
-        
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                
-                if(nums2[i]<nums2[j]){
-                    nxt[nums2[i]]=nums2[j];
-                    break;
-                }
-                else{
-                    nxt[nums2[i]]=-1;
-                }
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            
+            while(!st.empty() && st.top()<=nums2[i]){
+                st.pop();
             }
+            if(!st.empty()){
+                nxt[nums2[i]]=st.top();
+            }
+            else{
+                nxt[nums2[i]]=-1;
+            }
+            st.push(nums2[i]);
         }
-        nxt[nums2[n-1]]=-1;
         for(int i=0;i<m;i++){
-            ans.push_back(nxt[nums1[i]]);
+            nums1[i]=nxt[nums1[i]];
         }
-        return ans;
+        return nums1;
     }
 };
