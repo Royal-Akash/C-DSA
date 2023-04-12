3be4a0000289d281dbfc613ff8ee97e1cfbf22a1
Akash Jha <akashjha412@gmail.com>
2023-04-13 02:02:59 +0530

    Time: 13 ms (11.49%), Space: 10.6 MB (31.93%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        
+        stack<string>st;
+        string ans;
+        for(int i=0;i<path.size();i++){
+            
+            if(path[i]=='/') continue;
+            
+            string temp;
+            while(i<path.size() && path[i]!='/'){
+                temp+=path[i];
+                i++;
+            }
+            if(temp == "."){
+                continue;
+            }
+            if(temp==".."){
+                if(!st.empty()){
+                    st.pop();
+                }
+            }
+            else{
+                st.push(temp);
+            }
+            
+        }
+        if(st.empty()){
+            ans='/';
+        }
+        while(!st.empty()){
+            ans='/'+ st.top()+ans;
+            st.pop();
+        }
+        
+        return ans;
+    }
+};
