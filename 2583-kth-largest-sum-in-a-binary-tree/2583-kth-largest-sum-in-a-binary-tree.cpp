dafce611ea1990ad0b32a34a0965d748ee736b2b
Akash Jha <akashjha412@gmail.com>
2023-04-15 12:03:16 +0530

    Time: 229 ms (30.76%), Space: 96.2 MB (99.64%) - LeetHub

--- 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.cpp
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.cpp
@@ -9,44 +9,69 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     long long kthLargestLevelSum(TreeNode* root, int k) {
+//         priority_queue<long long>pq;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(!q.empty()){
+            
+//             int size = q.size();
+//             long long sum = 0;
+            
+//             for(int i=0;i<size;i++){
+                
+//               TreeNode* node = q.front();
+//                 q.pop();
+//                 sum+=node->val;
+//                 if(node->left){
+//                     q.push(node->left);
+//                 }
+//                 if(node->right){
+//                     q.push(node->right);
+//                 }
+//             }
+//             pq.push(sum);
+//         }
+//         if(k>pq.size()){
+//             return -1;
+//         }
+//         else{
+//             for(int i=0;i<k-1;i++){
+//                 pq.pop();
+//             }
+//         }
+        
+//         return pq.top();
+        
+//     }
+// };
+
 class Solution {
 public:
     long long kthLargestLevelSum(TreeNode* root, int k) {
-        priority_queue<long long>pq;
-        queue<TreeNode*>q;
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        
+        queue<TreeNode*> q;
         q.push(root);
-        while(!q.empty()){
+        int cnt = 0;
+        
+        while(!q.empty()) {
+            long long n = q.size(), s = 0;
             
-            int size = q.size();
-            long long sum = 0;
-            for(int i=0;i<size;i++){
+            while(n--) {
+                TreeNode* temp = q.front(); q.pop();
+                s += temp -> val;
                 
-              TreeNode* node = q.front();
-                q.pop();
-                // pq.push(node->val);
-                sum+=node->val;
-                if(node->left){
-                    q.push(node->left);
-                    // kthLargestLevelSum(root->left, k);
-                }
-                if(node->right){
-                    q.push(node->right);
-                    // kthLargestLevelSum(root->right,  k);
-                }
-                  
+                if(temp -> left) q.push(temp -> left);
+                if(temp -> right) q.push(temp -> right);
             }
-            pq.push(sum);
-        }
-        if(k>pq.size()){
-            return -1;
-        }
-        else{
-            for(int i=0;i<k-1;i++){
-                pq.pop();
-            }
+            cnt++;
+            pq.push(s);
+            if(pq.size() > k) pq.pop();
         }
         
-        return pq.top();
-        
+        return cnt < k ? -1 : pq.top();
     }
 };
