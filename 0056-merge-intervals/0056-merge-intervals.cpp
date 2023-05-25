3bde519b23a3c0d048d0bc9f329d32bac410b517
Akash Jha <akashjha412@gmail.com>
2023-05-25 23:06:05 +0530

    Time: 43 ms (53.08%), Space: 19.1 MB (43.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,8 +6,8 @@ public:
         sort(intervals.begin(), intervals.end());
         vector<vector<int>>ans;
         vector<int>temp = intervals[0];
-        if(n==1){
-            ans.push_back(temp);
+        if(n<=1){
+            return intervals;
         }
         for(int i=1;i<n;i++){
             if(temp[1]>=intervals[i][0] && temp[1]<=intervals[i][1]){
@@ -15,19 +15,14 @@ public:
                 
             }
             else if(temp[1]>=intervals[i][0] && temp[1]>=intervals[i][1]){
-                if(i==n-1){
-                ans.push_back(temp);
-            }
                 continue;
             }
             else{
                 ans.push_back(temp);
                 temp=intervals[i];
             }
-            if(i==n-1){
-                ans.push_back(temp);
-            }
         }
+        ans.push_back(temp);
         return ans;
     }
 };
