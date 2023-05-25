f0a68475524e25fda0879f73e0c57475d20cd1fd
Akash Jha <akashjha412@gmail.com>
2023-05-26 01:03:50 +0530

    Time: 40 ms (69.51%), Space: 19.1 MB (43.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,24 +5,29 @@ public:
         int n = intervals.size();
         sort(intervals.begin(), intervals.end());
         vector<vector<int>>ans;
-        vector<int>temp = intervals[0];
-        if(n<=1){
-            return intervals;
-        }
-        for(int i=1;i<n;i++){
-            if(temp[1]>=intervals[i][0]){
-                temp[1]=max(temp[1], intervals[i][1]);
-                
+        // vector<int>temp = intervals[0];
+        // if(n<=1){
+        //     return intervals;
+        // }
+        // for(int i=1;i<n;i++){
+        //     if(temp[1]>=intervals[i][0]){
+        //         temp[1]=max(temp[1], intervals[i][1]);
+        //     }
+        //     else{
+        //         ans.push_back(temp);
+        //         temp=intervals[i];
+        //     }
+        // }
+        // ans.push_back(temp);
+        // return ans;
+        for(int i=0;i<n;i++){
+            if(ans.empty() || ans.back()[1]<intervals[i][0]){
+                ans.push_back(intervals[i]);
             }
-            // else if(temp[1]>=intervals[i][0] && temp[1]>=intervals[i][1]){
-            //     continue;
-            // }
             else{
-                ans.push_back(temp);
-                temp=intervals[i];
+                ans.back()[1]=max(ans.back()[1], intervals[i][1]);
             }
         }
-        ans.push_back(temp);
         return ans;
     }
 };
