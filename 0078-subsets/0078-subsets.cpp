6980e4b6f3c59ae452795268d30871a175171b54
Akash Jha <akashjha412@gmail.com>
2023-07-15 16:47:08 +0530

    Time: 4 ms (48.50%), Space: 16.9 MB (11.32%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,17 +1,46 @@
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>>all_subset;
+//         int n = nums.size();
+//         for(int i=0;i<(1<<n);i++){
+//             vector<int>ans;
+//             for(int j=0;j<n;j++){
+//                 if(i&(1<<j)){
+//                     ans.push_back(nums[j]);
+//                 }
+//             }
+//             all_subset.push_back(ans);
+//         }
+//         return all_subset;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>all_subset;
-        int n = nums.size();
-        for(int i=0;i<(1<<n);i++){
-            vector<int>ans;
-            for(int j=0;j<n;j++){
-                if(i&(1<<j)){
-                    ans.push_back(nums[j]);
-                }
-            }
-            all_subset.push_back(ans);
+    
+    void sum(int ind, vector<int>sum1, vector<int>nums, int n, vector<vector<int>>&ans){
+        if(ind>=n){
+            ans.push_back(sum1);
+            return;
         }
-        return all_subset;
+        
+        sum(ind+1, sum1, nums, n, ans);
+        sum1.push_back(nums[ind]);
+        sum(ind+1, sum1, nums, n, ans);
+        sum1.pop_back();
+    }
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<vector<int>>ans;
+        vector<int>sum1;
+        sum(0, sum1, nums, n, ans);
+        return ans;
+        
     }
 };
