764d3eff6e7e0f5b8e73feb3972a9679a5dd1fca
Akash Jha <akashjha412@gmail.com>
2023-07-24 21:44:11 +0530

    Time: 491 ms (7.98%), Space: 6 MB (82.08%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,12 +1,35 @@
 class Solution {
 public:
+    string nxtPermutation(string s, int n){
+        int ind=-1;
+        for(int i=n-2;i>=0;i--){
+            if(s[i]<s[i+1]){
+                ind=i;
+                break;
+            }
+        }
+        if(ind!=-1){
+            for(int i=n-1;i>=ind;i--){
+                if(s[i]>s[ind]){
+                    swap(s[i], s[ind]);
+                    break;
+                }
+            }
+            reverse(s.begin()+ind+1, s.end());
+        }
+        else{
+            reverse(s.begin(), s.end());
+        }
+        return s;
+        
+    }
     string getPermutation(int n, int k) {
         string v;
         for(int i=1;i<=n;i++){
             v+=to_string(i);
         }
         for(int i=1;i<k;i++){
-            next_permutation(v.begin(), v.end());
+            v=nxtPermutation(v, n);
         }
         return v;
     }
