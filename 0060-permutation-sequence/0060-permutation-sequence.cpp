9034e7b3b63a4bf37d98a31855d193ad052e137a
Akash Jha <akashjha412@gmail.com>
2023-07-24 21:25:47 +0530

    Time: 173 ms (20.81%), Space: 6 MB (44.55%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int>v;
+        string v;
         for(int i=1;i<=n;i++){
-            v.push_back(i);
+            v+=to_string(i);
         }
         for(int i=1;i<k;i++){
             next_permutation(v.begin(), v.end());
         }
-        string ans;
-        for(int i=0;i<n;i++){
-            ans+=to_string(v[i]);
-        }
-        return ans;
+        return v;
     }
 };
