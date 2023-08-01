f23f2a715bca6bec33fb629e6dca3d3725eee153
Akash Jha <akashjha412@gmail.com>
2023-08-01 14:00:51 +0530

    Time: 12 ms (94.34%), Space: 13.7 MB (33.04%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -7,9 +7,14 @@ public:
             mp[nums[i]]++;
         }
         vector<int>ans;
-        priority_queue<pair<int, int>>pq;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        
         for(auto it:mp){
             pq.push({it.second, it.first});
+            if (pq.size() > k) {
+                pq.pop();
+            }
+            
         }
         for(int i=0;i<k;i++){
             pair<int, int>num=pq.top();
