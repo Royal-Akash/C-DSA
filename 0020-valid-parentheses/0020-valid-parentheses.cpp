4f44cb186dbb045a3ba1a37fdacc9df9a0e23d37
Akash Jha <akashjha412@gmail.com>
2023-08-06 19:37:53 +0530

    Time: 3 ms (40.96%), Space: 6.3 MB (35.45%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>s1;
+        int n = s.size();
+        s1.push(s[n-1]);
+        for(int i=n-2;i>=0;i--){
+            
+            if(s1.empty()){
+                s1.push(s[i]);
+            }
+            else if(s[i]=='(' && s1.top()==')'){
+                s1.pop();
+            }
+            else if(s[i]=='[' && s1.top()==']'){
+                s1.pop();
+            }
+            else if(s[i]=='{' && s1.top()=='}'){
+                s1.pop();
+            }
+            else{
+                s1.push(s[i]);
+            }
+        }
+        if(!s1.empty()) return false;
+        return true;
+    }
+};
