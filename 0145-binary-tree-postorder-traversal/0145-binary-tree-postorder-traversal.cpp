568ce89b4048eca6503a2288c0fed1c127177eb7
Akash Jha <akashjha412@gmail.com>
2023-08-18 21:06:40 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (21.90%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,28 +9,62 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Using single stack
+//         vector<int>ans;
+//         if(root==NULL) return ans;
+//         stack<TreeNode*>st;
+//         st.push(root);
+        
+//         while(!st.empty()){
+            
+//             root=st.top();
+//             st.pop();
+//             ans.push_back(root->val);
+            
+//             if(root->left){
+//                 st.push(root->left);
+//             }
+//             if(root->right){
+//                 st.push(root->right);
+//             }
+//         }
+        
+//         // root , right , left
+        
+//         reverse(ans.begin(), ans.end());
+        
+//         // After reverse -> left, right, root
+        
+//         return ans;
+
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
+        
+        stack<TreeNode*>st1;
+        stack<TreeNode*>st2;
         vector<int>ans;
         if(root==NULL) return ans;
-        stack<TreeNode*>st;
-        st.push(root);
-        
-        while(!st.empty()){
+        st1.push(root);
+        while(!st1.empty()){
+            root=st1.top();
+            st1.pop();
             
-            root=st.top();
-            st.pop();
-            ans.push_back(root->val);
-            
+            st2.push(root);
             if(root->left){
-                st.push(root->left);
+                st1.push(root->left);
             }
             if(root->right){
-                st.push(root->right);
+                st1.push(root->right);
             }
         }
-        reverse(ans.begin(), ans.end());
+        
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
         return ans;
+        
     }
 };
