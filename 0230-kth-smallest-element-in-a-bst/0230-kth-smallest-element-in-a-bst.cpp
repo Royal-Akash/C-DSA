1fa74ef9336ceada0d5b4310720763fde62504f1
Akash Jha <akashjha412@gmail.com>
2023-08-29 01:15:49 +0530

    Time: 15 ms (68.30%), Space: 24.2 MB (46.82%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,17 +11,53 @@
  */
 class Solution {
 public:
-    void findin(TreeNode* root, vector<int>&inorder){
+//     void findin(TreeNode* root, vector<int>&inorder){
         
-        if(root==NULL) return;
-        findin(root->left, inorder);
-        inorder.push_back(root->val);
-        findin(root->right, inorder);
+//         if(root==NULL) return;
+//         findin(root->left, inorder);
+//         inorder.push_back(root->val);
+//         findin(root->right, inorder);
         
-    }
+//     }
     int kthSmallest(TreeNode* root, int k) {
-        vector<int>inorder;
-        findin(root, inorder);
-        return inorder[k-1];
+        // vector<int>inorder;
+        // findin(root, inorder);
+        // return inorder[k-1];
+        if(root==NULL) return -1;
+        TreeNode* curr = root;
+        int count = 0;
+        int ans = -1;
+        while(curr!=NULL){
+            
+            if(curr->left==NULL){
+                count++;
+                if(count==k){
+                    ans=curr->val;
+                }
+                curr=curr->right;
+            }
+            else{
+                
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=NULL;
+                    count++;
+                    if(count==k){
+                        ans = curr->val;
+                    }
+                    curr=curr->right;
+                }
+                
+            }
+            
+        }
+        return ans;
     }
 };
