8fb6df461f087b61e197d0e0098b81c169874fb2
Akash Jha <akashjha412@gmail.com>
2023-08-29 14:37:39 +0530

    Time: 34 ms (68.71%), Space: 36.8 MB (47.86%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,30 +9,67 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator{
+    stack<TreeNode*>st;
+    bool flag = true;
+public:
+    
+    BSTIterator(TreeNode*root, bool direction){
+        flag=direction;
+        st.push(root);
+        pushall(root, flag);
+    }
+    
+    int next(bool direction){
+        TreeNode*temp = st.top();
+        TreeNode*ans = st.top();
+        st.pop();
+        if(direction==false && ans->right){
+            st.push(ans->right);
+            ans=ans->right;
+            pushall(ans, direction);
+        }
+        else if(direction==true && ans->left){
+            st.push(ans->left);
+            ans=ans->left;
+            pushall(ans, direction);
+        }
+        return temp->val;
+    }
+    
+    void pushall(TreeNode* root, bool direction){
+        if(direction==true){
+            while(root->right){
+                st.push(root->right);
+                root=root->right;
+            }
+        }
+        else{
+            while(root->left){
+                st.push(root->left);
+                root=root->left;
+            }
+        }
+    }
+    
+};
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
         if(root==NULL) return false;
-        unordered_map<int, int>mp;
-        stack<TreeNode*>st;
-        st.push(root);
-        while(!st.empty()){
-            
-            TreeNode*node = st.top();
-            st.pop();
-            int sol = k-node->val;
-            if(mp.count(sol)){
-                return true;
-                break;
+        BSTIterator l(root, false);
+        BSTIterator r(root, true);
+        
+        int left = l.next(false);
+        int right = r.next(true);
+        while(left<right){
+            if(left+right==k) return true;
+            else if(left+right<k){
+                left = l.next(false);
             }
-            if(node->left){
-                st.push(node->left);
+            else{
+                right = r.next(true);
             }
-            if(node->right){
-                st.push(node->right);
-            }
-            mp[node->val]++;
-            
         }
         return false;
     }
