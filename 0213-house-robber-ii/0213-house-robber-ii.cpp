c1b4413243378030dd257b9aa1f10644cd0ccf4e
Akash Jha <akashjha412@gmail.com>
2023-09-02 01:56:34 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (13.36%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+    int find(vector<int>& temp, vector<int>&dp){
         
+        int n = temp.size();
+        dp[0]=temp[0];
+        for(int i=1;i<n;i++){
+            
+            int pick = temp[i];
+            if(i>1){
+                pick+=dp[i-2];
+            }
+            int notpick = dp[i-1];
+            
+            dp[i]=max(pick , notpick);
+            
+        }
+        return dp[n-1];
+        
+    }
+    int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return nums[0];
         if(n==2) return max(nums[0], nums[1]);
+        vector<int>temp1, temp2;
+        for(int i=0;i<n;i++){
+            if(i!=0){
+                temp1.push_back(nums[i]);
+            }
+            if(i!=n-1){
+                temp2.push_back(nums[i]);
+            }
+        }
         vector<int>dp(n-1, 0);
-        dp[0]=nums[0];
-        for(int i=1;i<n-1;i++){
-            int take = nums[i];
-            if(i>1){
-                take+=dp[i-2];
-            }
-            int notake = dp[i-1];
-            dp[i]=max(take, notake);
-        }
-        int max1 = dp[n-2];
-        
-        dp[0]=nums[1];
-        for(int i=2;i<n;i++){
-            int take = nums[i];
-            if(i>2){
-                take+=dp[i-3];
-            }
-            int notake = dp[i-2];
-            dp[i-1]=max(take, notake);
-        }
-        return max(max1, dp[n-2]);
+        // vector<int>dp2(n-1, 0);
+        int sum1 = find(temp1, dp);
+        int sum2 = find(temp2, dp);
+        return max(sum1 , sum2);
     }
 };
