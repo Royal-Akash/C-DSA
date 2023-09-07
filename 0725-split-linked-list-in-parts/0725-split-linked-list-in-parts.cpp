7d2bab9069b661dea80a43156c6b9048706be977
Akash Jha <akashjha412@gmail.com>
2023-09-07 10:36:51 +0530

    Time: 4 ms (88.41%), Space: 8.8 MB (46.43%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -8,96 +8,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-// class Solution {
-// public:
-//     vector<ListNode*> splitListToParts(ListNode* head, int k) {
-        
-//         ListNode* curr = head;
-//         int count = 1;
-//         while(curr!=NULL){
-//             curr=curr->next;
-//             count++;
-//         }
-//         curr=head;
-//         vector<ListNode*>ans;
-//         if(count<=k){
-//             for(int i=0;i<k;i++){
-//                 if(curr!=NULL){
-//                     ListNode* temp = new ListNode(curr->val);
-//                     ans.push_back(temp);
-//                     curr=curr->next;
-//                 }
-//                 else{
-//                     ListNode* temp = NULL;
-//                     ans.push_back(temp);
-//                 }
-//             }
-//         }
-//         else{
-//             int sum = count/k;
-//             int mod = count%k + sum;
-            
-//             for(int i=0;i<k;i++){
-                
-//                 if(ans.empty()){
-//                     ListNode* temp = head;
-//                     ListNode* sol = temp;
-//                     for(int i=0;i<mod;i++){
-//                         ListNode* num = new ListNode(curr->val);
-//                         temp=num;
-//                         temp=temp->next;
-//                         curr=curr->next;
-//                     }
-//                     temp = NULL;
-//                     ans.push_back(temp);
-//                 }
-//                 else if (curr!=NULL){
-//                     ListNode* temp = NULL;
-//                     for(int j=0;j<sum;j++){
-//                         if(curr!=NULL){
-//                             ListNode* num = new ListNode(curr->val);
-//                             temp=num;
-//                             temp=temp->next;
-//                             curr=curr->next;
-//                         }
-//                     }
-//                     ans.push_back(temp);
-//                 }
-//             }
-            
-//         }
-//         return ans;
-        
-//     }
-// };
-
 class Solution {
 public:
-    int countNodes(ListNode* head) {
-        int n = 0;
-        while (head) {
-            n++;
-            head = head->next;
+    int countnodes(ListNode* head){
+        int count = 0;
+        while(head!=NULL){
+            head=head->next;
+            count++;
         }
-        return n;
+        return count;
     }
-    
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
-        int n = countNodes(head);
-        int part_size = n / k, left = n % k;
-        ListNode *ptr = head, *prev = NULL;
         
-        vector<ListNode*> res(k);
-        for (int i = 0; i < k; i++) {
-            res[i] = head;
-            
-            for (int j = 0; j < part_size + (i < left); j++) {
-                prev = head; 
-                head = head->next; 
+        
+        int count = countnodes(head);
+        vector<ListNode*>ans(k);
+        if(head==NULL) return ans;
+        int split = count/k, left = count%k;
+        ListNode* prev = NULL;
+        for(int i=0;i<k;i++){
+            ans[i]=head;
+            for(int j=0;j<split + (i<left);j++){
+                prev = head;
+                head=head->next;
             }
-            if (prev) prev->next = NULL;
+            prev->next=NULL;
+            
         }
+        return ans;
         
-        return res;
     }
 };
