cbb46b5ddbc06a98b978f2225e3429cd6db97a6b
Akash Jha <akashjha412@gmail.com>
2023-09-08 19:24:18 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (64.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,20 +2,39 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         
-        int dp[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 || j==0) dp[i][j]=1;
-                else{
-                    int up = 0;
-                    int left = 0;
-                    if(j>0) up=dp[i][j-1];
-                    if(i>0) left = dp[i-1][j];
+//         int dp[m][n];
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i==0 || j==0) dp[i][j]=1;
+//                 else{
+//                     int up = 0;
+//                     int left = 0;
+//                     if(j>0) up=dp[i][j-1];
+//                     if(i>0) left = dp[i-1][j];
 
-                    dp[i][j]=up+left;
+//                     dp[i][j]=up+left;
+//                 }
+//             }
+//         }
+//         return dp[m-1][n-1];
+        
+        vector<int>prev(n, 0);
+        for(int i=0;i<m;i++){
+            vector<int>curr(n, 0);
+            for(int j=0;j<n;j++){
+                
+                if(i==0 || j==0) curr[j]=1;
+                else{
+                    int up = 0, left = 0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
+                    
+                    curr[j]=up+left;
+                    
                 }
             }
+            prev= curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
