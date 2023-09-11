d105010eba0094b92d2b8bd149c585d242b7ffd0
Akash Jha <akashjha412@gmail.com>
2023-09-11 10:49:53 +0530

    Time: 2 ms (57.50%), Space: 6.1 MB (44.30%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int countOrders(int n) {
         
-        // const long long mod = 1e9+7;
-        // vector<long long>dp(n+1, 0);
-        // dp[1]=1*1ll;
-        // dp[2]=6*1ll;
-        // for(int i=3;i<=n;i++){
-        //     long long sum = (2ll*i-1)*i;
-        //     dp[i]=(dp[i-1]*sum)%mod;
-        // }
-        // return (int)dp[n];
-        long long MODULO = 1e9 + 7, ans = 1;
-        for (int i = 2; i <= n; i++) {
-            int spaceNum = (i-1)*2 + 1;
-            ans *= spaceNum * (spaceNum + 1) / 2;
-            ans %= MODULO;
+        long long mod = 1e9+7;
+        vector<long long>dp(501, 0);
+        dp[1]=1*1ll;
+        dp[2]=6*1ll;
+        for(int i=3;i<=n;i++){
+            int sum = (2ll*i-1)*i;
+            dp[i]=(dp[i-1]*sum)%mod;
         }
-        return (int) ans;
+        return (int)dp[n];
+        // long long MODULO = 1e9 + 7, ans = 1;
+        // for (int i = 2; i <= n; i++) {
+        //     int spaceNum = (i-1)*2 + 1;
+        //     ans *= spaceNum * (spaceNum + 1) / 2;
+        //     ans %= MODULO;
+        // }
+        // return (int) ans;
     }
 };
