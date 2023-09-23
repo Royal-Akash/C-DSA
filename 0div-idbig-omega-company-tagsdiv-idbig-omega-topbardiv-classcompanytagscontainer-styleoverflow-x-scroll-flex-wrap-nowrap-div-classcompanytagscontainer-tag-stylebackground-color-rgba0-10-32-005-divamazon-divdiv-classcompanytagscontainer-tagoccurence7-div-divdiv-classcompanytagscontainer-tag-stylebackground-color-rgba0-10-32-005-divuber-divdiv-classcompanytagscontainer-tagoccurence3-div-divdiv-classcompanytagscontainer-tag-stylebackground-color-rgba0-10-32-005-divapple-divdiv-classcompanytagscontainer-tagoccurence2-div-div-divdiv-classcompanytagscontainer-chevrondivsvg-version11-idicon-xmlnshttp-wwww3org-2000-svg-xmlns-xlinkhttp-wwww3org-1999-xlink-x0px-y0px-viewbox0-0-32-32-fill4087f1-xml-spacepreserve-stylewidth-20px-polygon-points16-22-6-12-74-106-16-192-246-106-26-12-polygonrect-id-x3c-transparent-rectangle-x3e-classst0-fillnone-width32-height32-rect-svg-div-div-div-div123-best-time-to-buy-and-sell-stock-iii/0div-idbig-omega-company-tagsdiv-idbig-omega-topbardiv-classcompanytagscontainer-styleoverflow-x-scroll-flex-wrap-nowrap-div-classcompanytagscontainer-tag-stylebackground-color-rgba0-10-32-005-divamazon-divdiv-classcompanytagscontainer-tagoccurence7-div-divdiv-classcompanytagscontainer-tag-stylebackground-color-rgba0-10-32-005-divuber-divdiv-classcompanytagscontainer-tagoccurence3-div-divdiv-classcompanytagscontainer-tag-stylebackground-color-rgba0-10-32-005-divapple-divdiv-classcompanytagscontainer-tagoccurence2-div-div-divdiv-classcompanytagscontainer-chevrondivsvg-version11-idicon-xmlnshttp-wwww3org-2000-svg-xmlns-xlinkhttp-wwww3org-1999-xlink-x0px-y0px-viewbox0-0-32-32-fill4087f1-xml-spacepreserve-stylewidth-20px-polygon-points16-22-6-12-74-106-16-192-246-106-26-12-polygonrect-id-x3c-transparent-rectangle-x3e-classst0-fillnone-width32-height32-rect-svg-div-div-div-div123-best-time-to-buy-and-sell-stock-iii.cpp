1f02a45e2d6d829f102bdc07f8c6690b84e6c008
Akash Jha <akashjha412@gmail.com>
2023-09-23 13:35:32 +0530

    Time: 479 ms (55.99%), Space: 207.5 MB (37.57%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div123-best-time-to-buy-and-sell-stock-iii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int max_trans = 2;
+        
+        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2, vector<int>(max_trans+1, 0)));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            
+            for(int buy=0;buy<=1;buy++){
+                
+                for(int cap=1;cap<=max_trans;cap++){
+                    int profit = 0;
+                    
+                    if(buy==1){
+                        profit = max(-prices[ind]+dp[ind+1][0][cap], dp[ind+1][1][cap]);
+                    }
+                    else{
+                        profit = max(prices[ind]+dp[ind+1][1][cap-1], dp[ind+1][0][cap]);
+                    }
+                    dp[ind][buy][cap]=profit;
+                }
+                
+            }
+            
+        }
+        
+        return dp[0][1][max_trans];
+        
+    }
+};
