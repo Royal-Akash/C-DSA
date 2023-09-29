933c86d9943cd97fa300dae8a5cb27edb3d9fd07
Akash Jha <akashjha412@gmail.com>
2023-09-29 09:37:35 +0530

    Time: 7 ms (12.32%), Space: 12.2 MB (7.68%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2335-minimum-amount-of-time-to-fill-cups/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fillCups(vector<int>& amount) {
+        
+        priority_queue<int>pq(amount.begin(), amount.end());
+        int count = 0;
+        while(pq.top()!=0){
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            a--, b--;
+            pq.push(a);
+            pq.push(b);
+            count++;
+        }
+        return count;
+    }
+};
