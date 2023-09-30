720044859e3fa3f124b31513e05906820bde2b18
Akash Jha <akashjha412@gmail.com>
2023-09-30 13:27:03 +0530

    Time: 84 ms (57.49%), Space: 13.6 MB (84.44%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -3,51 +3,42 @@ public:
     static bool comp(string &s1, string &s2){
         return s1.size()<s2.size();
     }
-    int longestStrChain(vector<string>& nums) {
-        int n = nums.size();
-        vector<int>dp(n, 1), hash(n);
-        sort(nums.begin(), nums.end(), comp);
+    bool compare(string & s1, string &s2){
         
-        int maxi = 1,lastInd=0;
-        for(int i=0;i<n;i++){
-            hash[i]=i;
-            for(int j=0;j<i;j++){
-                if(nums[j].size()+1==nums[i].size()){
+        if(s2.size()+1==s1.size()){
                     int w1=0, w2=0;
-                    while(w1<nums[j].size() && w2<nums[i].size()){
-                        if(nums[j][w1]==nums[i][w2]){
+                    while(w1<s2.size() && w2<s1.size()){
+                        if(s2[w1]==s1[w2]){
                             w1++, w2++;
                         }
                         else{
                             w2++;
                         }
                     }
-                    if(w1==nums[j].size()){
-                        dp[i]=max(dp[i], 1+dp[j]);
-                        hash[i]=j;
+                    if(w1==s2.size()){
+                        return true;
                     }
                     
                 }
-            }
-            if(maxi<dp[i]){
-                maxi=dp[i];
-                lastInd=i;
-            }
-        }
+        return false;
         
-        vector<string>temp;
-        temp.push_back(nums[lastInd]);
-        while(hash[lastInd]!=lastInd){
-            
-            lastInd=hash[lastInd];
-            temp.push_back(nums[lastInd]);
-            
+    }
+    int longestStrChain(vector<string>& nums) {
+        int n = nums.size();
+        vector<int>dp(n, 1);
+        sort(nums.begin(), nums.end(), comp);
+        
+        int maxi = 1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                
+                if(compare(nums[i], nums[j]) && dp[i]<1+dp[j]){
+                    dp[i]=1+dp[j];
+                }
+                
+            }
+            maxi = max(maxi , dp[i]);
         }
-        reverse(temp.begin(), temp.end());
-        // return temp;
-        for(auto it: temp){
-            cout<<it<<" ";
-        }cout<<endl;
         return maxi;
     }
 };
