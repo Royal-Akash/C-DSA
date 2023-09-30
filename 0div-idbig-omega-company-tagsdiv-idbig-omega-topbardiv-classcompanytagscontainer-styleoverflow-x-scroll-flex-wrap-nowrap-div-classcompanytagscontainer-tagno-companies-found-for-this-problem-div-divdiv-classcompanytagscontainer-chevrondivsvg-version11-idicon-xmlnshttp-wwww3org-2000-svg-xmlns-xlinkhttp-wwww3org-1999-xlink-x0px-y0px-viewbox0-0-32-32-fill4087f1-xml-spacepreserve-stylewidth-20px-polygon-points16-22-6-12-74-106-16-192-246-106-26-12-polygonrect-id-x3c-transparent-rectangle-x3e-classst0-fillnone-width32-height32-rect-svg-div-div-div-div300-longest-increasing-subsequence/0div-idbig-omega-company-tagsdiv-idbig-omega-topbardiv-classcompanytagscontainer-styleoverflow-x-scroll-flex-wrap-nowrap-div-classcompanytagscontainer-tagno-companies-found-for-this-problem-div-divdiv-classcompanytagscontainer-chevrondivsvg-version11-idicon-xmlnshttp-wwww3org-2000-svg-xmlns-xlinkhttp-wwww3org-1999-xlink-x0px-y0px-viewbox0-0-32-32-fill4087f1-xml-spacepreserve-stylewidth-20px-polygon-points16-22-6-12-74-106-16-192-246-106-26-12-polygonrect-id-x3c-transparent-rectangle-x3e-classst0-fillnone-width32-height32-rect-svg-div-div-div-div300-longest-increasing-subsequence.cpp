5a99eca740198be80309b5848030a994882ab82b
Akash Jha <akashjha412@gmail.com>
2023-09-30 11:28:58 +0530

    Time: 6 ms (90.13%), Space: 10.7 MB (51.22%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
@@ -2,38 +2,35 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         
-        int n = nums.size();
-//         vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
-        
-//         for(int i=n-1;i>=0;i--){
-            
-//             for(int prev = i-1;prev>=-1;prev--){
+        int n = nums.size();  
+//         if(n==1) return 1;
+//         int maxi = 0;
+//         vector<int>dp(n, 1);
+//         for(int i=1;i<n;i++){
+//             for(int j=0;j<i;j++){
                 
-//                 int len = dp[i+1][prev+1];
-                
-//                 if(prev==-1 || nums[i]>nums[prev+1]){
-//                     len = max(len, 1 + dp[i+1][i+1]);
+//                 if(nums[i]>nums[j]){
+//                     dp[i]=max(dp[i], 1+dp[j]);
 //                 }
-//                 dp[i][prev+1]=len;
 //             }
-            
+//             maxi=max(maxi, dp[i]);
 //         }
-//         return dp[0][0];
         
-        if(n==1) return 1;
-        int maxi = 0;
-        vector<int>dp(n, 1);
+//         return maxi;
+        
+        vector<int>temp;
+        temp.push_back(nums[0]);
         for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                
-                if(nums[i]>nums[j]){
-                    dp[i]=max(dp[i], 1+dp[j]);
-                }
+            
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
             }
-            maxi=max(maxi, dp[i]);
+            else{
+                int ind = lower_bound(temp.begin(), temp.end(), nums[i])-temp.begin();
+                temp[ind]=nums[i];
+            }
+            
         }
-        
-        return maxi;
-        
+        return temp.size();
     }
 };
